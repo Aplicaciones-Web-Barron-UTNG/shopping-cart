8a566824ed832ed7c461b807432e8529e9fd8039
gbarron2014 <gbarron@utng.edu.mx>
2023-01-23 09:31:46 -0600

    Clase 02

--- clase02/shopping-cart/src/app/app.component.ts
+++ clase02/shopping-cart/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {IProduct} from './interfaces/product.interface';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-cart';
+
+  products: IProduct[]; //Una lista de productos
+
+
+  constructor() {
+    this.products = [
+      {id:1, name:"Mascarilla 01", description: "Embellece tu cara 01", price:100, img:''},
+      {id:2, name:"Mascarilla 02", description: "Embellece tu cara 02", price:101, img:''},
+      {id:3, name:"Mascarilla 03", description: "Embellece tu cara 03", price:102, img:''},
+      {id:4, name:"Mascarilla 04", description: "Embellece tu cara 04", price:103, img:''},
+      {id:5, name:"Mascarilla 05", description: "Embellece tu cara 05", price:104, img:''},
+      {id:6, name:"Mascarilla 06", description: "Embellece tu cara 06", price:105, img:''}
+    ];
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
--- clase02/shopping-cart/src/app/product/product.component.ts
+++ clase02/shopping-cart/src/app/product/product.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IProduct } from '../interfaces/product.interface';
+import { ICart } from '../interfaces/cart.interface';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent{
+  @Input() product: IProduct;
+  cart:ICart[];
 
+  constructor() {
+    localStorage.setItem('cart', JSON.stringify([]));
+  }
+
+  ngOnInit(): void {
+    console.log(this.product);    
+  }
+
+    add():void {
+      localStorage.setItem('cart', JSON.stringify( this.product));
+    }
 }
