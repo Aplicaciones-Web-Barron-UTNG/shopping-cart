8c90a23074ead3bc406b453a52f7c06522ea3883
gbarron2014 <gbarron@utng.edu.mx>
2023-01-27 09:15:46 -0600

    Clase 03

--- clase03/shopping-cart/src/app/product/product.component.ts
+++ clase03/shopping-cart/src/app/product/product.component.ts
@@ -1,25 +1,40 @@
 import { Component, Input } from '@angular/core';
-import {IProduct, ICart} from '../interfaces';
+import { IProduct, ICart } from '../interfaces';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent{
+export class ProductComponent {
   @Input() product: IProduct;
-  cart:ICart[];
+  cart: ICart[];
 
   constructor() {
   }
 
   ngOnInit(): void {
-    console.log(this.product);    
+    console.log(this.product);
   }
 
-    add():void {
-      this.cart = JSON.parse(localStorage.getItem('cart') as string);
-      this.cart.push({name: this.product.name , price: this.product.price, quantity: 1});
-      localStorage.setItem('cart', JSON.stringify(this.cart));
+  add(): void {
+    this.cart = JSON.parse(localStorage.getItem('cart') as string);
+
+    //Validar si un producto ya existe
+    let isFound = false;
+    this.cart.forEach(item => {
+      if (item.id === this.product.id) {
+        isFound = true;
+        item.quantity = item.quantity + 1; //Aumenta en 1
+      }
+    });
+
+    if (!isFound) {
+      this.cart.push({ id: this.product.id, name: this.product.name, price: this.product.price, quantity: 1 });
+      
     }
+
+
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
 }
